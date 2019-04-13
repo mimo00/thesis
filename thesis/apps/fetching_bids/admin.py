939ffc67bae5e26b6a4d62c0d83a048e5b75dd33
mimo00 <michal.mokrogulski@gmail.com>
2019-04-13 18:02:53 +0200

    Make admin side more readable.

--- thesis/apps/fetching_bids/admin.py
+++ thesis/apps/fetching_bids/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 from apps.fetching_bids import models
 
-admin.site.register(models.ElectricVehicle)
+
+class ElectricVehicleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'max_battery_capacity', 'min_battery_capacity', 'max_charging_power', 'user')
+
+
+class ChargingLocalizationInline(admin.TabularInline):
+    model = models.ChargingLocalization
+    extra = 0
+
+
+class BidAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date', 'mode', 'electric_vehicle')
+    inlines = [
+        ChargingLocalizationInline
+    ]
+
+
+admin.site.register(models.ElectricVehicle, ElectricVehicleAdmin)
 admin.site.register(models.Node)
-admin.site.register(models.Generator)
+admin.site.register(models.Localization)
+admin.site.register(models.Bid, BidAdmin)
+admin.site.register(models.ChargingLocalization)
