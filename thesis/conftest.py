dd9bb0e16585da8305f3d15377a28ffd76348ba0
mimo00 <michal.mokrogulski@gmail.com>
2019-04-13 17:42:45 +0200

    Configure pytest.

---
+++ thesis/conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient
+
+from apps.fetching_bids.factories import UserFactory
+
+
+@pytest.fixture(scope="session")
+def api_client():
+    return APIClient()
+
+
+@pytest.fixture()
+def auth_api_client(db):
+    client = APIClient()
+    user = UserFactory()
+    token = Token.objects.create(user=user)
+    client.credentials(HTTP_AUTHORIZATION="Token " + token.key)
+    client.user = user
+    return client
