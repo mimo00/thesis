b73e9328e9dc3e272d07be1385f6850b76adb7f6
mimo00 <michal.mokrogulski@gmail.com>
2019-04-24 09:49:23 +0200

    Configure admin panel for bids decisions.

---
+++ thesis/apps/bids_decisions/admin.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from apps.bids_decisions import models
+
+
+class ChargingLocalizationDecisionInline(admin.TabularInline):
+    model = models.ChargingLocalizationDecision
+    extra = 0
+
+
+class BidDecisionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'decision', 'bid')
+
+    inlines = [
+        ChargingLocalizationDecisionInline
+    ]
+
+
+class AggregatorDecisionAdmin(admin.ModelAdmin):
+    list_display = ('id',  'decision', 'decision_date', 'receive_date')
+
+
+admin.site.register(models.BidDecision, BidDecisionAdmin)
+admin.site.register(models.AggregatorDecision, AggregatorDecisionAdmin)
