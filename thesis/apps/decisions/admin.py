b56fd95a2c6f507bcb1003d4accb7e55dfebcd2a
mimo00 <michal.mokrogulski@gmail.com>
2019-06-06 11:41:46 +0200

    Add admin site.

--- thesis/apps/decisions/admin.py
+++ thesis/apps/decisions/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from apps.decisions import models
 from django.contrib.auth.models import Group
 
-from apps.decisions.models import Offer, EnergyHourOffer
-
 
 class AggregatorDecisionAdmin(admin.ModelAdmin):
     list_display = ('id', 'decision_date', 'receive_date')
@@ -20,6 +18,4 @@ class ScheduleDecisionAdmin(admin.ModelAdmin):
 admin.site.register(models.AggregatorDecision, AggregatorDecisionAdmin)
 admin.site.register(models.AggregatorGroupDecision, AggregatorGroupDecisionAdmin)
 admin.site.register(models.ScheduleDecision, ScheduleDecisionAdmin)
-admin.site.register(Offer)
-admin.site.register(EnergyHourOffer)
 admin.site.unregister(Group)
